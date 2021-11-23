911daeeb7b440b6f48d3a14ccb8cca1cd6ac0963
James Woolfenden <james.woolfenden@gmail.com>
2021-11-23 20:37:50 +0000

    fail

--- rds.tf
+++ rds.tf
@@ -12,8 +12,4 @@ provider "aws" {
 resource "aws_db_subnet_group" "default" {
   name       = "main"
   subnet_ids = ["subnet-08d97e381dbc80d40","subnet-03fdfb13a135366a7"]
-
-  tags = {
-    Name = "My DB subnet group"
-  }
 }
