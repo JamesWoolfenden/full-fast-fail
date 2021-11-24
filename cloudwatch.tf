e7b15fecf93913284eb4252a1a8b9cdb7cb0e893
James Woolfenden <james.woolfenden@gmail.com>
2021-11-24 08:52:45 +0000

    fails

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,5 +1,15 @@
 resource "aws_cloudwatch_log_group" "logs" {
-  name              = var.log_group_name
-  tags              = var.common_tags
+  name = var.log_group_name
+  tags = var.common_tags
 }
 
+variable "log_group_name" {
+  default = "MyLogGroupName"
+}
+
+variable "common_tags" {
+  type = map(string)
+  default = {
+    automatedby = "JamesWoolfenden"
+  }
+}
--- rds.tf
+++ rds.tf
@@ -1,15 +1,15 @@
 resource "aws_rds_cluster" "fail2" {
   backup_retention_period = 0
-  db_subnet_group_name = aws_db_subnet_group.default.name
-  master_username = "james"
-  master_password = "password12345"
+  db_subnet_group_name    = aws_db_subnet_group.default.name
+  master_username         = "james"
+  master_password         = "password12345"
 }
 
 provider "aws" {
-  region="eu-west-2"
+  region = "eu-west-2"
 }
 
 resource "aws_db_subnet_group" "default" {
   name       = "main"
-  subnet_ids = ["subnet-08d97e381dbc80d40","subnet-03fdfb13a135366a7"]
+  subnet_ids = ["subnet-08d97e381dbc80d40", "subnet-03fdfb13a135366a7"]
 }
