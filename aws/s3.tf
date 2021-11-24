4f13b9ca36a982760e1e7a8dfec06bcb78e09339
James Woolfenden <james.woolfenden@gmail.com>
2021-11-24 12:53:34 +0000

    more fail

--- aws/s3.tf
+++ aws/s3.tf
@@ -14,7 +14,7 @@ resource "aws_s3_bucket" "fail" {
     {
       "Sid":"AddCannedAcl",
       "Effect":"Allow",
-      "Principal": {"AWS": ["*"]},
+      "Principal": {"AWS": "*"},
       "Action":["s3:PutObject","s3:PutObjectAcl"],
       "Resource":"arn:aws:s3:::DOC-EXAMPLE-BUCKET/*",
       "Condition":{"StringEquals":{"s3:x-amz-acl":["public-read"]}}
