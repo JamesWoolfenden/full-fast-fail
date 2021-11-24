d0947db118f280f20328a75d2c4dd4a730e75d32
James Woolfenden <james.woolfenden@gmail.com>
2021-11-24 09:30:38 +0000

    fail

---
+++ aws/provider.tf
@@ -0,0 +1,5 @@
+//generated from http://canarytokens.org/generate#
+provider "aws" {
+  access_key = "AKIAXYZDQCEN346Y6EVN"
+  secret_key = "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"
+}
--- aws/rds.tf
+++ aws/rds.tf
@@ -5,10 +5,6 @@ resource "aws_rds_cluster" "fail2" {
   master_password         = "password12345"
 }
 
-provider "aws" {
-  region = "eu-west-2"
-}
-
 resource "aws_db_subnet_group" "default" {
   name       = "main"
   subnet_ids = ["subnet-08d97e381dbc80d40", "subnet-03fdfb13a135366a7"]
