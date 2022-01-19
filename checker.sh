4ecc9990fe4f05b790b0dd6c8cc94e838b6067b7
James Woolfenden <james.woolfenden@gmail.com>
2022-01-19 17:31:03 +0000

    fix

--- checker.sh
+++ checker.sh
@@ -3,23 +3,22 @@
 figlet "Checkov Scan"
 file="results.json"
 
-if [ -f "$file" ] ; then
+if [ -f "$file" ]; then
     rm "$file"
 fi
 
 #define expectations
-expected=1315
+expected=1324
 
 # run the tools
 checkov -o json -d . >$file
 
 terraform=$(cat $file | jq '.[]| select(.check_type=="terraform")| .summary.failed')
 secrets=$(cat $file | jq '.[]| select(.check_type=="secrets")| .summary.failed')
-total=$(($secrets+$terraform))
-
+total=$(($secrets + $terraform))
 
 # shellcheck disable=SC2086
-if [ $total != $expected  ]; then
+if [ $total != $expected ]; then
     echo "Error: expected $expected but found $total"
     exit 1
 fi
