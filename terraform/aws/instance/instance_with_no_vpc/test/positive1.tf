b90bfd66fe4351a5801e9628fa534257466c12f5
James Woolfenden <James.Woolfenden@gmail.com>
2022-02-05 16:56:52 +0000

    move

--- terraform/aws/instance/instance_with_no_vpc/test/positive1.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_instance" "positive1" {
-  ami = "ami-003634241a8fcdec0"
-
-  instance_type = "t2.micro"
-
-}
--- terraform/aws/lambda/lambda_permission_misconfigured/test/positive.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_lambda_permission" "positive1" {
-  action        = "lambda:DeleteFunction"
-  function_name = aws_lambda_function.logging.function_name
-  principal     = "logs.eu-west-1.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_log_group.default.arn}:*"
-}
