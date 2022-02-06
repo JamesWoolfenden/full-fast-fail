7a3ed4b76945010c53bbf89e21680c3c7bcb7e9a
James Woolfenden <James.Woolfenden@gmail.com>
2022-02-06 09:28:04 +0000

    WIP

--- terraform/azurerm/secret_expiration_not_set/test/positive.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_key_vault_secret" "positive1" {
-    name         = "secret-sauce"
-    value        = "szechuan"
-    key_vault_id = azurerm_key_vault.example.id
-
-    tags = {
-    environment = "Production"
-    }
-}
--- terraform/google/ip_forwarding_enabled/test/positive.tf
+++
@@ -1,6 +0,0 @@
-#this is a problematic code where the query should report a result(s)
-data "google_compute_instance" "appserver" {
-  name = "primary-application-server"
-  zone = "us-central1-a"
-  can_ip_forward = true
-}
