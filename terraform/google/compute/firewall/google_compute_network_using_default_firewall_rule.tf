d410098539fe506865f957ced054c3d11dc60d7c
James Woolfenden <James.Woolfenden@gmail.com>
2022-02-06 20:54:21 +0000

    WIP

--- terraform/google/compute/firewall/google_compute_network_using_default_firewall_rule.tf
+++ terraform/google/compute/firewall/google_compute_network_using_default_firewall_rule.tf
@@ -4,7 +4,3 @@ resource "google_compute_firewall" "positive1" {
   name    = "default"
   network = google_compute_network.positive1.name
 }
-
-resource "google_compute_network" "positive1" {
-  name = "test-network"
-}
--- terraform/google/compute/security/google_compute_ssl_policy_weak_cipher_in_use.tf
+++ terraform/google/compute/security/google_compute_ssl_policy_weak_cipher_in_use.tf
@@ -8,9 +8,3 @@ resource "google_compute_ssl_policy" "positive1" {
   profile         = "CUSTOM"
   custom_features = ["TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384", "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"]
 }
-
-resource "google_compute_ssl_policy" "positive2" {
-  name            = "custom-ssl-policy"
-  profile         = "CUSTOM"
-  custom_features = ["TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384", "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"]
-}
