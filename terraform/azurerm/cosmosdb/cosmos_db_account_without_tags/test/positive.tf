cd96ed5c79248eeaae622c18c952ede1cb27274a
James Woolfenden <James.Woolfenden@gmail.com>
2022-02-06 12:55:09 +0000

    WIP

--- terraform/azurerm/cosmosdb/cosmos_db_account_without_tags/test/positive.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_cosmosdb_account" "positive1" {
-  name                = "tfex-cosmos-db-${random_integer.ri.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  offer_type          = "Standard"
-  kind                = "GlobalDocumentDB"
-}
