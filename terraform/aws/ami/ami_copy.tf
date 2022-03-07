13e3f509faf61646091ab0d84dd2e72796e45868
James Woolfenden <james.woolfenden@gmail.com>
2022-03-07 16:57:18 +0000

    WIP

--- terraform/aws/ami/ami_copy.tf
+++ terraform/aws/ami/ami_copy.tf
@@ -7,7 +7,7 @@ resource "aws_ami_copy" "example" {
   description       = "A copy of ami-xxxxxxxx"
   source_ami_id     = "ami-xxxxxxxx"
   source_ami_region = "us-west-1"
-  encrypted = false #default is false
+  encrypted         = false #default is false
   tags = {
     Name = "HelloWorld"
     test = "failed"
