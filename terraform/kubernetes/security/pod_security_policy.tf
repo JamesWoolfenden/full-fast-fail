69376004bfb7c8821fc7888e191388cc88079ddd
James Woolfenden <james.woolfenden@gmail.com>
2022-03-14 12:39:11 +0000

    more checks

--- terraform/kubernetes/security/pod_security_policy.tf
+++ terraform/kubernetes/security/pod_security_policy.tf
@@ -1,7 +1,8 @@
 # fails
-# CKV_K8S_3
-# Containers should not run with allowPrivilegeEscalationCheckov CKV_K8S_5
+# Do not admit containers wishing to share the host IPC namespaceCheckov CKV_K8S_3
+# Do not admit containers wishing to share the host process ID namespaceCheckov CKV_K8S_1
 # Do not admit containers wishing to share the host network namespaceCheckov CKV_K8S_4
+# Containers should not run with allowPrivilegeEscalationCheckov CKV_K8S_5
 resource "kubernetes_pod_security_policy" "fail" {
   metadata {
     name = "terraform-example"
@@ -21,9 +22,13 @@ resource "kubernetes_pod_security_policy" "fail" {
       "downwardAPI",
       "persistentVolumeClaim",
     ]
-
+    # required_drop_capabilities = [
+    #   "NET_RAW",
+    #   "KILL",
+    #   "SYS_TIME",
+    # ]
     run_as_user {
-      rule = "MustRunAsNonRoot"
+      rule = "RunAsAny"
     }
 
     se_linux {
